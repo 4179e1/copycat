b464ce7dc7441ee6b065d69cf0dccfc1a503e951
lyre <lyre@poetpalace.org>
2013-04-30 22:43:35 +0800

    implemented seek & ioctl

--- scull/scull.h
+++ scull/scull.h
@@ -63,4 +63,32 @@ long scull_ioctl (struct file *filp, unsigned int cmd, unsigned long arg);
 int scull_open (struct inode *inode, struct file *filp);
 int scull_release (struct inode *inode, struct file *filep);
 
+#define SCULL_IOC_MAGIC 'k'
+
+#define SCULL_IOCRESET _IO(SCULL_IOC_MAGIC, 0)
+/*
+ * S means 'Set' trough a ptr
+ * T means 'Tell' directly with the argument value
+ * G means 'Get' reply by setting through a ptr
+ * Q means 'Query' response is on the return value
+ * X means 'eXchange' switch G and S atomically
+ * H means 'sHift' switch T and Q atomically
+ */
+
+ #define SCULL_IOCSQUANTUM	_IOW(SCULL_IOC_MAGIC, 1, int)
+ #define SCULL_IOCSQSET 	_IOW(SCULL_IOC_MAGIC, 2, int)
+ #define SCULL_IOCTQUANTUM	_IO(SCULL_IOC_MAGIC, 3)
+ #define SCULL_IOCTQSET 	_IO(SCULL_IOC_MAGIC, 4)
+ #define SCULL_IOCGQUANTUM	_IOR(SCULL_IOC_MAGIC, 5, int)
+ #define SCULL_IOCGQSET 	_IOR(SCULL_IOC_MAGIC, 6, int)
+ #define SCULL_IOCQQUANTUM 	_IO(SCULL_IOC_MAGIC, 7)
+ #define SCULL_IOCQQSET		_IO(SCULL_IOC_MAGIC, 8)
+ #define SCULL_IOCXQUANTUM	_IOWR(SCULL_IOC_MAGIC, 9, int)
+ #define SCULL_IOCXQSET		_IOWR(SCULL_IOC_MAGIC, 10, int)
+ #define SCULL_IOCHQUANTUM	_IO(SCULL_IOC_MAGIC, 11)
+ #define SCULL_IOCHQSET		_IO(SCULL_IOC_MAGIC, 12)
+ #define SCULL_P_IOCTSIZE	_IO(SCULL_IOC_MAGIC, 13)
+ #define SCULL_P_IOCQSIZE	_IO(SCULL_IOC_MAGIC, 14)
+ #define SCULL_IOC_MAXNR 	14
+
 #endif /* _SCULL_H_ */
