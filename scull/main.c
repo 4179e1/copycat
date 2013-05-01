2a26ab27f3c09e43702ad332dfcfbba6246f1274
lyre <lyre@poetpalace.org>
2013-05-01 23:06:02 +0800

    implemented proc filesystem

--- scull/main.c
+++ scull/main.c
@@ -1,5 +1,7 @@
 #include <linux/module.h>
 #include <asm/uaccess.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
 #include "scull.h"
 
 int scull_major = SCULL_MAJOR;
@@ -348,6 +350,111 @@ int scull_trim (struct scull_dev *dev)
 	return 0;
 }
 
+static void *scull_seq_start (struct seq_file *s, loff_t *pos)
+{
+	if (*pos >= scull_nr_devs)
+		return NULL;
+	return scull_devices + *pos;
+}
+
+static void *scull_seq_next (struct seq_file *s, void *v, loff_t *pos)
+{
+	(*pos)++;
+	if (*pos >= scull_nr_devs)
+		return NULL;
+	return scull_devices + *pos;
+}
+
+static void scull_seq_stop (struct seq_file *s, void *v)
+{
+}
+
+static int scull_seq_show (struct seq_file *s, void *v)
+{
+	struct scull_dev *dev = (struct scull_dev *)v;
+	struct scull_qset *d;
+	int i;
+
+	if (down_interruptible (&dev->sem))
+		return -ERESTARTSYS;
+	seq_printf (s, "\nDevice %i; qset %i, q %i, sz %li\n", (int) (dev - scull_devices), dev->qset, dev->quantum, dev->size);
+	for (d = dev->data; d; d = d->next)
+	{
+		seq_printf (s, "  item at %p, qset at %p\n", d, d->data);
+		if (d->data && !d->next) /* dump only the last item */
+			for (i = 0; i < dev->qset; i++)
+				if (d->data[i])
+					seq_printf (s, "    % 4i: %8p\n", i, d->data[i]);
+	}
+	up (&dev->sem);
+	return 0;
+}
+
+static struct seq_operations scull_seq_ops = 
+{
+	.start = scull_seq_start,
+	.next = scull_seq_next,
+	.stop = scull_seq_stop,
+	.show = scull_seq_show,
+};
+
+static int scull_proc_open (struct inode *inode, struct file *file)
+{
+	return seq_open (file, &scull_seq_ops);
+}
+
+static struct file_operations scull_proc_ops = 
+{
+	.owner 		= THIS_MODULE,
+	.open 		= scull_proc_open,
+	.read		= seq_read,
+	.llseek		= seq_lseek,
+	.release	= seq_release,
+};
+
+int scull_read_procmem (char *buf, char **start, off_t offset, int count, int *eof, void *data)
+{
+	int i, j, len = 0;
+	int limit = count - 80;
+
+	for (i = 0; i < scull_nr_devs && len <= limit; i++)
+	{
+		struct scull_dev *d = &scull_devices[i];
+		struct scull_qset *qs = d->data;
+		if (down_interruptible (&d->sem))
+			return -ERESTARTSYS;
+		len += sprintf (buf+len, "\nDevice %i,: qset %i, q %i, sz %li\n", i, d->qset, d->quantum, d->size);
+		for (; qs && len <= limit; qs = qs->next) /* travesal list */
+		{
+			len += sprintf (buf + len, "  item at %p, qset at %p\n", qs, qs->data);
+			if (qs->data && !qs->next)	/* dump last qset */
+				for (j = 0; j < d->qset; j++)
+					if (qs->data[j])
+						len += sprintf (buf + len, "   % 4i: %8p\n", j, qs->data[j]);
+		}
+		up(&scull_devices[i].sem);
+	}
+	*eof = 1;
+	return len;
+}
+
+static void scull_create_proc (void)
+{
+	struct proc_dir_entry *entry;
+	create_proc_read_entry ("scullmem", 0 /* default mode */, 
+			NULL /* partent dir */, scull_read_procmem,
+			NULL /*client data */);
+	entry = create_proc_entry ("scullseq", 0, NULL);
+	if (entry)
+		entry->proc_fops = &scull_proc_ops;
+}
+
+static void scull_remove_proc (void)
+{
+	remove_proc_entry ("scullmem", NULL);
+	remove_proc_entry ("scullseq", NULL);
+}
+
 void scull_cleanup_module (void)
 {
 	int i;
@@ -364,6 +471,10 @@ void scull_cleanup_module (void)
 		}
 		kfree (scull_devices);
 	}
+
+#ifdef SCULL_DEBUG 
+	scull_remove_proc ();
+#endif /* SCULL_DEBUG */
 
 	unregister_chrdev_region (devno, scull_nr_devs);
 }
@@ -405,6 +516,10 @@ int scull_init_module (void)
 		sema_init (&scull_devices[i].sem, 1);
 		scull_setup_cdev (&scull_devices[i], i);
 	}
+
+#ifdef SCULL_DEBUG
+	scull_create_proc ();
+#endif /* SCULL_DEBUG */
 
 	return 0;
 
